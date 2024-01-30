1fa63845ca386d4f6a78643de1bc7a57a63df666
eric <eric444555@gmail.com>
2024-01-30 16:29:57 +0800

    new control flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { FooComponent } from './components/foo.component';
+import { ViewportLazyComponent } from './components/viewport-lazy.component';
+import { ItemComponent } from './components/item.component';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { of } from 'rxjs';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    FooComponent,
+    ViewportLazyComponent,
+    ItemComponent,
+    AsyncPipe,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular17-demo';
+  index = 0;
+  show = true;
+  obs$ = of('hello');
+  message = 'hello';
+  items: { id: number; label: string }[] = [
+    { id: 1, label: 'foo' },
+    { id: 2, label: 'bar' },
+    { id: 3, label: 'baz' },
+    { id: 4, label: 'hello' },
+    { id: 5, label: 'world' },
+  ];
 }
