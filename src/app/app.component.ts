1eceebf34a2f3a473a78c71171ad8b6e2f6c8652
eric <eric444555@gmail.com>
2024-02-06 16:27:01 +0800

    upload mutiple sheet test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { AsyncPipe, CommonModule } from '@angular/common';
 import { of } from 'rxjs';
 import { RouterOutlet } from '@angular/router';
 import { ExampleComponent } from './components/example.component';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -42,4 +43,23 @@ export class AppComponent {
     { id: 4, label: 'hello' },
     { id: 5, label: 'world' },
   ];
+
+  onFileChange(evt: any) {
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+      
+      // 循環遍歷每個 sheet
+      wb.SheetNames.forEach(sheetName => {
+        const ws: XLSX.WorkSheet = wb.Sheets[sheetName];
+        // 使用 XLSX.utils.sheet_to_json 讀取 sheet 內容
+        const data = XLSX.utils.sheet_to_json(ws);
+        console.log(sheetName, data); // 這裡可以看到每個工作表的內容
+      });
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
 }
